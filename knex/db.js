45e83de68125947917ba99e20e30447630cf9fa7
James Sangalli <j.l.sangalli@gmail.com>
2016-06-03 17:28:07 +1200

    fixed knex issue

--- knex/db.js
+++ knex/db.js
@@ -1,6 +1,6 @@
+var env = process.env.NODE_ENV || 'development'
 var config = require('./knexfile.js')
 var knex = require('knex')(config[env])
-var env = process.env.NODE_ENV || 'development'
 
 module.exports = {
   getCharities:() => {
